dfd8fb91ab65cf0ff07a8e7333dad3002eb968ff
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-07-23 10:53:21 -0300

    Incluindo frame embed do youtube

--- frontend/ionic/src/app/tabs-page/next/tabs-page-next.component.ts
+++ frontend/ionic/src/app/tabs-page/next/tabs-page-next.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Live } from 'src/app/shared/model/live.model';
 import { LiveService } from 'src/app/shared/service/live.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-tab-page-next',
@@ -9,13 +10,21 @@ import { LiveService } from 'src/app/shared/service/live.service';
 export class TabsPageNextComponent implements OnInit{
     public lives: Live[];
 
-    constructor(private liveService: LiveService){
+    urlSafe: SafeResourceUrl;
+    
+    constructor(
+        private liveService: LiveService, 
+        private ds: DomSanitizer){
         
     }
+    
     ngOnInit(): void {
         this.liveService.getLivesWithFlag('next')
         .subscribe(data => {
             this.lives = data.content;
-        })
+            this.lives.forEach(live => {
+                live.urlSafe = this.ds.bypassSecurityTrustResourceUrl(live.liveLink);
+            });
+        });
     }
 }
--- frontend/ionic/src/app/tabs-page/previous/tabs-page-previous.component.ts
+++ frontend/ionic/src/app/tabs-page/previous/tabs-page-previous.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Live } from 'src/app/shared/model/live.model';
 import { LiveService } from 'src/app/shared/service/live.service';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-tab-page-previous',
@@ -9,14 +10,21 @@ import { LiveService } from 'src/app/shared/service/live.service';
 export class TabsPagePreviousComponent implements OnInit{
     
     public lives: Live[];
+    urlSafe: SafeResourceUrl;
 
-    constructor(private liveService: LiveService){
+    constructor(
+        private liveService: LiveService,
+        private ds: DomSanitizer){
         
     }
+
     ngOnInit(): void {
         this.liveService.getLivesWithFlag('previous')
         .subscribe(data => {
             this.lives = data.content;
-        })
+            this.lives.forEach(live => {
+                live.urlSafe = this.ds.bypassSecurityTrustResourceUrl(live.liveLink);
+            });
+        });
     }
 }
